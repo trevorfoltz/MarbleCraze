7e1685606adcf0029cd145df3a3114b6536e5669
Trevor Foltz <trevorfoltz@gmail.com>
2013-10-27 01:04:42 -0700

    Initial Commit

---
+++ MarbleCraze/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MarbleCraze
+//
+//  Created by Trevlord on 10/27/13.
+//  Copyright (c) 2013 forevorware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MarbleCraze/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MarbleCraze
+//
+//  Created by Trevlord on 10/27/13.
+//  Copyright (c) 2013 forevorware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
